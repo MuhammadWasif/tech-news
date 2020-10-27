9d26e90f9b99a0e86f1784e467f3817c53af3e7f
MuhammadWasif <wasif33@outlook.com>
2020-10-27 12:03:42 +0500

    🚀 initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
+const mongose = require('mongoose');
+const { importSchema } = require('graphql-import');
+const typeDefs = importSchema('./schema.graphql');
+
+const resolvers = {
+  Query: {
+    message: () => 'Hello, World!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongose.connection.once('open', () => {
+  server.listen().then((host) => {
+    console.log(`🚀 Server running at ${host.url}`);
+  });
+});
