ee3618b453f9fdb95a6de282d5d136a69ddec068
MuhammadWasif <wasif33@outlook.com>
2020-11-10 19:28:21 +0500

    📝 additional raw changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Auth, User, Home } from './pages';
+import { Auth, User, Home, New } from './pages';
 
 import './scss/index.scss';
 
@@ -9,6 +9,7 @@ function App() {
       <Route exact path='/' component={Home} />
       <Route exact path='/u/:username' component={User} />
       <Route exact path='/auth' component={Auth} />
+      <Route exact path='/new' component={New} />
     </Router>
   );
 }
