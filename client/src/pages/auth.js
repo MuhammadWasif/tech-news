4eea35bd511670bf0a74112d79031c39be743098
MuhammadWasif <wasif33@outlook.com>
2020-11-17 20:44:12 +0500

    style auth (login + signup) page

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,18 +1,24 @@
 import { useState, useContext } from 'react';
 import { useMutation } from '@apollo/client';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 
 import { LOGIN, SIGNUP } from '../graphql/mutations';
 import Header from '../components/header';
+import Loader from '../components/loader';
 import { GlobalContext } from '../context/GlobalState';
 
 function Auth(props) {
   const [creds, setCreds] = useState({});
+  const [page, setPage] = useState('login');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const { setLoggedInUser, state } = useContext(GlobalContext);
 
   // mutations
   const [login] = useMutation(LOGIN);
   const [signup] = useMutation(SIGNUP);
+
+  const history = useHistory();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,6 +32,7 @@ function Auth(props) {
     e.preventDefault();
 
     try {
+      setLoading(true);
       const { data } = await login({
         variables: {
           username: creds.username,
@@ -45,8 +52,12 @@ function Auth(props) {
         id: data.login.user.id,
         username: data.login.user.username,
       });
+
+      history.push('/');
     } catch (error) {
       console.log(error);
+      setError(error.message);
+      setLoading(false);
     }
   };
 
@@ -54,26 +65,36 @@ function Auth(props) {
     e.preventDefault();
     const { username, email, password } = creds;
     try {
+      setLoading(true);
       const { data } = await signup({
         variables: { username, email, password },
       });
-
-      localStorage.setItem('auth_token', data.login.token);
+      console.log(data);
+      localStorage.setItem('auth_token', data.createUser.token);
       localStorage.setItem(
         'loggedin_user_tech_news',
         JSON.stringify({
-          id: data.login.user.id,
-          username: data.login.user.username,
+          id: data.createUser.user.id,
+          username: data.createUser.user.username,
         })
       );
 
       await setLoggedInUser({
-        id: data.login.user.id,
-        username: data.login.user.username,
+        id: data.createUser.user.id,
+        username: data.createUser.user.username,
       });
+      history.push('/');
     } catch (error) {
       console.log(error);
+      setError(error.message);
+      setLoading(false);
     }
+  };
+
+  const activeStyles = {
+    backgroundColor: '#ea5b0c',
+    color: 'white',
+    boxShadow: 'none',
   };
 
   const { loggedInUser } = state;
@@ -82,25 +103,96 @@ function Auth(props) {
   return (
     <div>
       <Header />
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <input type='text' name='username' onChange={handleChange} />
-          <input type='password' name='password' onChange={handleChange} />
 
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
+      <div className='auth'>
+        <div className='auth__btn-group'>
+          <button
+            className='auth__btn-group--btn'
+            style={page === 'login' ? activeStyles : {}}
+            onClick={() => setPage('login')}
+          >
+            Login
+          </button>
+          <button
+            className='auth__btn-group--btn'
+            style={page === 'register' ? activeStyles : {}}
+            onClick={() => setPage('register')}
+          >
+            Register
+          </button>
+        </div>
 
-      <div>
-        <h2>Register</h2>
-        <form onSubmit={handleSignup}>
-          <input type='text' name='username' onChange={handleChange} />
-          <input type='email' name='email' onChange={handleChange} />
-          <input type='password' name='password' onChange={handleChange} />
+        {page === 'login' ? (
+          <div className='auth__login'>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+              <input
+                type='text'
+                name='username'
+                placeholder='Username'
+                onChange={handleChange}
+                required
+              />
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                onChange={handleChange}
+                required
+              />
 
-          <button type='submit'>Sign Up</button>
-        </form>
+              <button type='submit' disabled={loading}>
+                {loading ? <Loader /> : 'Login'}
+              </button>
+              {error ? (
+                <p>
+                  <b>Error: </b>
+                  {error}
+                </p>
+              ) : null}
+            </form>
+          </div>
+        ) : (
+          <div>
+            <h2>Register</h2>
+            <form onSubmit={handleSignup}>
+              <input
+                type='text'
+                name='username'
+                placeholder='Username'
+                onChange={handleChange}
+                required
+                pattern='[^\s]+'
+              />
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                onChange={handleChange}
+                required
+                minLength={6}
+              />
+              <input
+                type='email'
+                name='email'
+                placeholder='E-mail'
+                onChange={handleChange}
+                required
+              />
+
+              <button type='submit' disabled={loading}>
+                {loading ? <Loader /> : 'Sign Up'}
+              </button>
+
+              {error ? (
+                <p>
+                  <b>Error: </b>
+                  {error}
+                </p>
+              ) : null}
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
